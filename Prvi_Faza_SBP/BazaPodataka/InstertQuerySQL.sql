52078356ed81dde12d991b6cf988457cdb2bc019
Mateja <mateja.nesic13@gmail.com>
2025-09-14 02:27:26 +0200

    Prva faza apdejt, dodate su skripte za kreiranje

--- Prvi_Faza_SBP/BazaPodataka/InstertQuerySQL.sql
+++ Prvi_Faza_SBP/BazaPodataka/InstertQuerySQL.sql
@@ -1,112 +1,116 @@
 INSERT ALL
-INTO Cudoviste (IdCudovista, NazivCud, PodTipCud, VekPomCud) VALUES (1, 'Vampir', 'Natprirodno bice', 200)
-INTO Cudoviste (IdCudovista, NazivCud, PodTipCud, VekPomCud) VALUES (2, 'Grifon', 'Mitsko bice', 150)
-INTO Cudoviste (IdCudovista, NazivCud, PodTipCud, VekPomCud) VALUES (3, 'Sirena', 'Mitsko bice', 500)
-INTO Cudoviste (IdCudovista, NazivCud, PodTipCud, VekPomCud) VALUES (4, 'Banshee', 'Duša iz vatre', 50)
-INTO Cudoviste (IdCudovista, NazivCud, PodTipCud, VekPomCud) VALUES (5, 'Golem', 'Veštaćko bice', 100)
+INTO Cudoviste (IdCudovista, NazivCud, PodTipCud, VekPomCud) VALUES (1, 'Zmaj', 'Mitsko bice', 1200)
+INTO Cudoviste (IdCudovista, NazivCud, PodTipCud, VekPomCud) VALUES (2, 'Vukodlak', 'Natprirodno bice', 400)
+INTO Cudoviste (IdCudovista, NazivCud, PodTipCud, VekPomCud) VALUES (3, 'Vampir', 'Natprirodno bice', 600)
+INTO Cudoviste (IdCudovista, NazivCud, PodTipCud, VekPomCud) VALUES (4, 'Vila Raviojla', 'Mitsko bice', 150)
+INTO Cudoviste (IdCudovista, NazivCud, PodTipCud, VekPomCud) VALUES (5, 'Meduza', 'Mitsko bice', 300)
 SELECT * FROM dual;
 
 INSERT ALL
 INTO MagCudoviste (IdCudovista, Postojanje) VALUES (1, 'Da')
-INTO MagCudoviste (IdCudovista, Postojanje) VALUES (2, 'Da')
-INTO MagCudoviste (IdCudovista, Postojanje) VALUES (3, 'Da')
+INTO MagCudoviste (IdCudovista, Postojanje) VALUES (4, 'Da')
+INTO MagCudoviste (IdCudovista, Postojanje) VALUES (5, 'Da')
 SELECT * FROM dual;
 
 INSERT ALL
-INTO NeMagCudoviste (IdCudovista, Visina, Duzina, Kandze, BrojGlava, ZiviUVodi, Leti, Otrovno, Tezina) VALUES (1, 180, 200, 'Oštre', 1, 'Da', 'Ne', 'Da', 70)
-INTO NeMagCudoviste (IdCudovista, Visina, Duzina, Kandze, BrojGlava, ZiviUVodi, Leti, Otrovno, Tezina) VALUES (5, 120, 150, 'Oštre', 0, 'Da', 'Ne', 'Ne', 80)
+INTO NeMagCudoviste (IdCudovista, Visina, Duzina, Kandze, BrojGlava, ZiviUVodi, Leti, Otrovno, Tezina) VALUES (2, 200, 220, 'Oštre', 1, 'Ne', 'Ne', 'Ne', 80)
+INTO NeMagCudoviste (IdCudovista, Visina, Duzina, Kandze, BrojGlava, ZiviUVodi, Leti, Otrovno, Tezina) VALUES (3, 180, 200, 'Oštre', 1, 'Ne', 'Ne', 'Da', 70)
 SELECT * FROM dual;
 
 INSERT ALL
-INTO SpecSposobnost (IdSpecSpos, NazivSpecSpos) VALUES (1, 'Teleportacija', 1)
-INTO SpecSposobnost (IdSpecSpos, NazivSpecSpos) VALUES (5, 'Infravisio', 5)
+INTO SpecSposobnost (IdSpecSpos, NazivSpecSpos) VALUES (1, 'Vatreni dah')
+INTO SpecSposobnost (IdSpecSpos, NazivSpecSpos) VALUES (2, 'Transformacija u vuka')
+INTO SpecSposobnost (IdSpecSpos, NazivSpecSpos) VALUES (3, 'Hipnotički pogled')
+INTO SpecSposobnost (IdSpecSpos, NazivSpecSpos) VALUES (4, 'Leteće čari')
+INTO SpecSposobnost (IdSpecSpos, NazivSpecSpos) VALUES (5, 'Pogled u kamen')
 SELECT * FROM dual;
 
 INSERT ALL
-INTO MagSposobnost (IdMagSpos, NazivMagSpos, OpisMagSpos, Ofanzivna, Defanzivna) VALUES (2, 'Led', 'Magija leda', 'Da', 'Da')
-INTO MagSposobnost (IdMagSpos, NazivMagSpos, OpisMagSpos, Ofanzivna, Defanzivna) VALUES (3, 'Elektricitet', 'Magija elektriciteta', 'Da', 'Ne')
-INTO MagSposobnost (IdMagSpos, NazivMagSpos, OpisMagSpos, Ofanzivna, Defanzivna) VALUES (4, 'Zemlja', 'Magija zemlje', 'Ne', 'Da')
+INTO MagSposobnost (IdMagSpos, NazivMagSpos, OpisMagSpos, Ofanzivna, Defanzivna) VALUES (1, 'Vatrena magija', 'Zmaj vatreni udar', 'Da', 'Ne')
+INTO MagSposobnost (IdMagSpos, NazivMagSpos, OpisMagSpos, Ofanzivna, Defanzivna) VALUES (2, 'Mesečeva magija', 'Vukodlak mesečeva moć', 'Ne', 'Da')
+INTO MagSposobnost (IdMagSpos, NazivMagSpos, OpisMagSpos, Ofanzivna, Defanzivna) VALUES (3, 'Krvi magija', 'Vampir moć krvi', 'Da', 'Da')
+INTO MagSposobnost (IdMagSpos, NazivMagSpos, OpisMagSpos, Ofanzivna, Defanzivna) VALUES (4, 'Prirodna magija', 'Vila Raviojla leteće čari', 'Ne', 'Da')
+INTO MagSposobnost (IdMagSpos, NazivMagSpos, OpisMagSpos, Ofanzivna, Defanzivna) VALUES (5, 'Kamen pogled', 'Meduza pogubljujući pogled', 'Da', 'Da')
 SELECT * FROM dual;
 
 INSERT ALL
-INTO Bajalica (IdBajalice, NazivBajalice, Zastita, Prednost) VALUES (2, 'Ogledalo istine', 'Reflektuje magijske napade', 'Otkriva laži')
-INTO Bajalica (IdBajalice, NazivBajalice, Zastita, Prednost) VALUES (3, 'Elixir zdravlja', 'Povećava regeneraciju', 'Brže ozdravljenje')
-INTO Bajalica (IdBajalice, NazivBajalice, Zastita, Prednost) VALUES (4, 'Amajlija za disanje pod vodom', 'Omogućava disanje pod vodom', 'Omogućava boravak pod vodom')
+INTO Bajalica (IdBajalice, NazivBajalice, Zastita, Prednost) VALUES (1, 'Plameni štit', 'Štiti od vatre', 'Odbija napade')
+INTO Bajalica (IdBajalice, NazivBajalice, Zastita, Prednost) VALUES (2, 'Mesečev talisman', 'Povećava snagu vukodlaka', 'Omogućava kontrolu transformacije')
+INTO Bajalica (IdBajalice, NazivBajalice, Zastita, Prednost) VALUES (3, 'Krvni amulet', 'Štiti od vampira', 'Povećava moć')
+INTO Bajalica (IdBajalice, NazivBajalice, Zastita, Prednost) VALUES (4, 'Krila vila', 'Omogućava letenje', 'Povećava magične sposobnosti')
+INTO Bajalica (IdBajalice, NazivBajalice, Zastita, Prednost) VALUES (5, 'Ogledalo Meduze', 'Reflektuje pogled', 'Štiti od kamena')
 SELECT * FROM dual;
 
 INSERT ALL
-INTO LovacTragaPoznatimPred (IdLovacTragaPP, ImePP, IdLov) VALUES (1, 'Drakula', 1)
-INTO LovacTragaPoznatimPred (IdLovacTragaPP, ImePP, IdLov) VALUES (2, 'Griffin Reinhart', 2)
-INTO LovacTragaPoznatimPred (IdLovacTragaPP, ImePP, IdLov) VALUES (3, 'Ariel', 3)
-INTO LovacTragaPoznatimPred (IdLovacTragaPP, ImePP, IdLov) VALUES (4, 'Sladja', 4)
-INTO LovacTragaPoznatimPred (IdLovacTragaPP, ImePP, IdLov) VALUES (5, 'iron Golem', 5)
+INTO LovacTragaPoznatimPred (IdLovacTragaPP, ImePP, IdLov) VALUES (1, 'Smaug', 1)
+INTO LovacTragaPoznatimPred (IdLovacTragaPP, ImePP, IdLov) VALUES (2, 'Remus', 2)
+INTO LovacTragaPoznatimPred (IdLovacTragaPP, ImePP, IdLov) VALUES (3, 'Drakula', 3)
+INTO LovacTragaPoznatimPred (IdLovacTragaPP, ImePP, IdLov) VALUES (4, 'Raviojla', 4)
+INTO LovacTragaPoznatimPred (IdLovacTragaPP, ImePP, IdLov) VALUES (5, 'Persej', 5)
 SELECT * FROM dual;
 
-
 INSERT ALL
-INTO Legenda (IdLegende, PrvoPominjanje, TekstLegende, ZemljaPorekla, ImePoznatogPred) VALUES (1, '1000 BC', 'Legenda o vampiru', 'Rumunia', 'Drakula')
-INTO Legenda (IdLegende, PrvoPominjanje, TekstLegende, ZemljaPorekla, ImePoznatogPred) VALUES (2, '500 BC', 'Legenda o grifonu', 'America', 'Griffin Reinhart')
-INTO Legenda (IdLegende, PrvoPominjanje, TekstLegende, ZemljaPorekla, ImePoznatogPred) VALUES (3, '200 AD', 'Legenda o sireni', 'Okean', 'Ariel')
-INTO Legenda (IdLegende, PrvoPominjanje, TekstLegende, ZemljaPorekla, ImePoznatogPred) VALUES (4, '700 AD', 'Legenda o banshee', 'Irska', 'Sladja')
-INTO Legenda (IdLegende, PrvoPominjanje, TekstLegende, ZemljaPorekla, ImePoznatogPred) VALUES (5, '1500 AD', 'Legenda o golemu', 'Engleska', 'iron Golem')
+INTO Legenda (IdLegende, PrvoPominjanje, TekstLegende, ZemljaPorekla, ImePoznatogPred) VALUES (1, '1000 BC', 'Legenda o Zmaju', 'Kina', 'Smaug')
+INTO Legenda (IdLegende, PrvoPominjanje, TekstLegende, ZemljaPorekla, ImePoznatogPred) VALUES (2, '800 BC', 'Legenda o Vukodlaku', 'Evropa', 'Remus')
+INTO Legenda (IdLegende, PrvoPominjanje, TekstLegende, ZemljaPorekla, ImePoznatogPred) VALUES (3, '500 BC', 'Legenda o Vampiru', 'Rumunija', 'Drakula')
+INTO Legenda (IdLegende, PrvoPominjanje, TekstLegende, ZemljaPorekla, ImePoznatogPred) VALUES (4, '1500 AD', 'Legenda o Vili Ravljoli', 'Francuska', 'Ravljola')
+INTO Legenda (IdLegende, PrvoPominjanje, TekstLegende, ZemljaPorekla, ImePoznatogPred) VALUES (5, '700 BC', 'Legenda o Meduzi', 'Grčka', 'Persej')
 SELECT * FROM dual;
 
-
 INSERT ALL
+INTO PoznatiPredstavnik (JedinstvenoIme, Starost, IdCud) VALUES ('Smaug', 1000, 1)
+INTO PoznatiPredstavnik (JedinstvenoIme, Starost, IdCud) VALUES ('Remus', 220, 2)
 INTO PoznatiPredstavnik (JedinstvenoIme, Starost, IdCud) VALUES ('Drakula', 532, 3)
-INTO PoznatiPredstavnik (JedinstvenoIme, Starost, IdCud) VALUES ('Griffin Reinhart', 210, 2)
-INTO PoznatiPredstavnik (JedinstvenoIme, Starost, IdCud) VALUES ('Ariel', 300, 4)
-INTO PoznatiPredstavnik (JedinstvenoIme, Starost, IdCud) VALUES ('Sladja', 150, 5)
-INTO PoznatiPredstavnik (JedinstvenoIme, Starost, IdCud) VALUES ('iron Golem', 200, 1)
+INTO PoznatiPredstavnik (JedinstvenoIme, Starost, IdCud) VALUES ('Raviojla', 150, 4)
+INTO PoznatiPredstavnik (JedinstvenoIme, Starost, IdCud) VALUES ('Persej', 300, 5)
 SELECT * FROM dual;
 
 INSERT ALL
-INTO Lokacija (IdLokacije, TipLok, NazivLok, ZemljaLok, Blago, IdLeg) VALUES (1, 'Šuma', 'Transilvanija', 'Rumunija', 'Dragoceni metali', 1)
-INTO Lokacija (IdLokacije, TipLok, NazivLok, ZemljaLok, Blago, IdLeg) VALUES (2, 'Planina', 'Himalaji', 'Indija', 'Retki kristali', 2)
-INTO Lokacija (IdLokacije, TipLok, NazivLok, ZemljaLok, Blago, IdLeg) VALUES (3, 'More', 'Egejsko more', 'Grcka', 'Skupocene perle', 3)
-INTO Lokacija (IdLokacije, TipLok, NazivLok, ZemljaLok, Blago, IdLeg) VALUES (4, 'Pećina', 'Black Forest', 'Nemačka', 'Stari artefakti', 4)
-INTO Lokacija (IdLokacije, TipLok, NazivLok, ZemljaLok, Blago, IdLeg) VALUES (5, 'Močvara', 'Loch Ness', 'Škotska', 'Zlatnik iz doba Vikinzi', 5)
+INTO Lokacija (IdLokacije, TipLok, NazivLok, ZemljaLok, Blago, IdLeg) VALUES (1, 'Planina', 'Krasna planina', 'Kina', 'Zlatni dragulji', 1)
+INTO Lokacija (IdLokacije, TipLok, NazivLok, ZemljaLok, Blago, IdLeg) VALUES (2, 'Šuma', 'Pun mesec', 'Evropa', 'Retki kristali', 2)
+INTO Lokacija (IdLokacije, TipLok, NazivLok, ZemljaLok, Blago, IdLeg) VALUES (3, 'Dvorac', 'Transilvanija', 'Rumunija', 'Blago i zlato', 3)
+INTO Lokacija (IdLokacije, TipLok, NazivLok, ZemljaLok, Blago, IdLeg) VALUES (4, 'Livada', 'Raviojlina dolina', 'Francuska', 'Magične biljke', 4)
+INTO Lokacija (IdLokacije, TipLok, NazivLok, ZemljaLok, Blago, IdLeg) VALUES (5, 'Pećina', 'Gorgonska pećina', 'Grčka', 'Oružje i artefakti', 5)
 SELECT * FROM dual;
 
 INSERT ALL
-INTO Zastita (IdZastite, TipZastite, NazivZastite, IdLok) VALUES (1, 'Fizička', 'Štit', 1)
-INTO Zastita (IdZastite, TipZastite, NazivZastite, IdLok) VALUES (2, 'Magijska', 'Ogledalo', 2)
-INTO Zastita (IdZastite, TipZastite, NazivZastite, IdLok) VALUES (3, 'Zdravstvena', 'Elixir', 3)
-INTO Zastita (IdZastite, TipZastite, NazivZastite, IdLok) VALUES (4, 'Magijska', 'Amajlija', 4)
-INTO Zastita (IdZastite, TipZastite, NazivZastite, IdLok) VALUES (5, 'Fizička', 'Odelo', 5)
-SELECT * FROM dual;
-
-
-INSERT ALL
-INTO Lovac (IdLovca, ImeLovca) VALUES (1, 'Van Helsing')
-INTO Lovac (IdLovca, ImeLovca) VALUES (2, 'Persej')
-INTO Lovac (IdLovca, ImeLovca) VALUES (3, 'Sam Winchester')
-INTO Lovac (IdLovca, ImeLovca) VALUES (4, 'Hercules')
-INTO Lovac (IdLovca, ImeLovca) VALUES (5, 'Geralt of Rivia')
+INTO Zastita (IdZastite, TipZastite, NazivZastite, IdLok) VALUES (1, 'Fizička', 'Zmaj štit', 1)
+INTO Zastita (IdZastite, TipZastite, NazivZastite, IdLok) VALUES (2, 'Fizička', 'Srebrna oklop', 2)
+INTO Zastita (IdZastite, TipZastite, NazivZastite, IdLok) VALUES (3, 'Magijska', 'Krvni amulet', 3)
+INTO Zastita (IdZastite, TipZastite, NazivZastite, IdLok) VALUES (4, 'Magijska', 'Vila čarolija', 4)
+INTO Zastita (IdZastite, TipZastite, NazivZastite, IdLok) VALUES (5, 'Magijska', 'Ogledalo Meduze', 5)
 SELECT * FROM dual;
 
 INSERT ALL
-INTO Protivmere (IdProtivmere, NazivProtivmere, OpisProtivmere) VALUES (1, 'Sveta voda', 'Efikasna protiv vampira')
-INTO Protivmere (IdProtivmere, NazivProtivmere, OpisProtivmere) VALUES (2, 'Srebrni metak', 'Uobičajeno oružje protiv vukodlaka')
-INTO Protivmere (IdProtivmere, NazivProtivmere, OpisProtivmere) VALUES (3, 'Šiljci od češnjaka', 'Odbija vampire')
-INTO Protivmere (IdProtivmere, NazivProtivmere, OpisProtivmere) VALUES (4, 'Svetlo', 'Oštetljivo za mračna bića')
-INTO Protivmere (IdProtivmere, NazivProtivmere, OpisProtivmere) VALUES (5, 'Čarobni pepeo', 'Neutrališe čini')
+INTO Lovac (IdLovca, ImeLovca) VALUES (1, 'Eragon')
+INTO Lovac (IdLovca, ImeLovca) VALUES (2, 'Jacob Black')
+INTO Lovac (IdLovca, ImeLovca) VALUES (3, 'Van Helsing')
+INTO Lovac (IdLovca, ImeLovca) VALUES (4, 'Tinker Bell')
+INTO Lovac (IdLovca, ImeLovca) VALUES (5, 'Persej')
 SELECT * FROM dual;
 
 INSERT ALL
-INTO Predmet (IdPredmeta, TipPredmeta, NazivPredmeta, MaterijalPredmeta) VALUES (1, 'Oružje', 'Mač', 'Čelik')
-INTO Predmet (IdPredmeta, TipPredmeta, NazivPredmeta, MaterijalPredmeta) VALUES (2, 'Oružje', 'Sekira', 'Bronza')
-INTO Predmet (IdPredmeta, TipPredmeta, NazivPredmeta, MaterijalPredmeta) VALUES (3, 'Amajlija', 'Krst', 'Drvo')
-INTO Predmet (IdPredmeta, TipPredmeta, NazivPredmeta, MaterijalPredmeta) VALUES (4, 'Svetiljka', 'Laterna', 'Bakar')
-INTO Predmet (IdPredmeta, TipPredmeta, NazivPredmeta, MaterijalPredmeta) VALUES (5, 'Magični prah', 'Eliksir', 'Mirisna sol')
+INTO Protivmere (IdProtivmere, NazivProtivmere, OpisProtivmere) VALUES (1, 'Zmajeva koplja', 'Efikasna protiv Zmaja')
+INTO Protivmere (IdProtivmere, NazivProtivmere, OpisProtivmere) VALUES (2, 'Srebrni metak', 'Efikasan protiv Vukodlaka')
+INTO Protivmere (IdProtivmere, NazivProtivmere, OpisProtivmere) VALUES (3, 'Svetlo i beli češnjak', 'Efikasno protiv Vampira')
+INTO Protivmere (IdProtivmere, NazivProtivmere, OpisProtivmere) VALUES (4, 'Magijski prah', 'Efikasan protiv Vila')
+INTO Protivmere (IdProtivmere, NazivProtivmere, OpisProtivmere) VALUES (5, 'Ogledalo', 'Efikasno protiv Meduze')
 SELECT * FROM dual;
 
 INSERT ALL
-INTO UsloviZaPrimenu (IdUslova, Uslov, IdProtivM) VALUES (1, 'Naneti na neprijatelja', 1)
-INTO UsloviZaPrimenu (IdUslova, Uslov, IdProtivM) VALUES (2, 'Pogoditi vukodlaka', 2)
-INTO UsloviZaPrimenu (IdUslova, Uslov, IdProtivM) VALUES (3, 'Približiti se vampiru', 3)
-INTO UsloviZaPrimenu (IdUslova, Uslov, IdProtivM) VALUES (4, 'Upaliti svetiljku', 4)
-INTO UsloviZaPrimenu (IdUslova, Uslov, IdProtivM) VALUES (5, 'Raspršiti magični prah', 5)
+INTO Predmet (IdPredmeta, TipPredmeta, NazivPredmeta, MaterijalPredmeta) VALUES (1, 'Oružje', 'Plamenik', 'Čelik')
+INTO Predmet (IdPredmeta, TipPredmeta, NazivPredmeta, MaterijalPredmeta) VALUES (2, 'Oružje', 'Srebrni bodež', 'Srebro')
+INTO Predmet (IdPredmeta, TipPredmeta, NazivPredmeta, MaterijalPredmeta) VALUES (3, 'Amajlija', 'Amulet krvi', 'Zlato')
+INTO Predmet (IdPredmeta, TipPredmeta, NazivPredmeta, MaterijalPredmeta) VALUES (4, 'Magični predmet', 'Čarobni prah', 'Bilje')
+INTO Predmet (IdPredmeta, TipPredmeta, NazivPredmeta, MaterijalPredmeta) VALUES (5, 'Ogledalo', 'Ogledalo Meduze', 'Staklo')
+SELECT * FROM dual;
+
+INSERT ALL
+INTO UsloviZaPrimenu (IdUslova, Uslov, IdProtivM) VALUES (1, 'Ubosti Zmaja kopljem', 1)
+INTO UsloviZaPrimenu (IdUslova, Uslov, IdProtivM) VALUES (2, 'Pogoditi Vukodlaka srebrnim metkom', 2)
+INTO UsloviZaPrimenu (IdUslova, Uslov, IdProtivM) VALUES (3, 'Približiti se Vampiru sa svetlom i češnjakom', 3)
+INTO UsloviZaPrimenu (IdUslova, Uslov, IdProtivM) VALUES (4, 'Raspršiti magični prah na Vilu', 4)
+INTO UsloviZaPrimenu (IdUslova, Uslov, IdProtivM) VALUES (5, 'Reflektovati pogled Meduze ogledalom', 5)
 SELECT * FROM dual;
 
 INSERT ALL
@@ -118,30 +122,31 @@ INTO ProtivmeraPosedujePredmet (IdPPP, IdPred, IdPM) VALUES (5, 5, 5)
 SELECT * FROM dual;
 
 INSERT ALL
+INTO BajalicaProtivMagCud (IdBaj, IdCud, IdBajalicaPMagCud) VALUES (1, 1, 1)
 INTO BajalicaProtivMagCud (IdBaj, IdCud, IdBajalicaPMagCud) VALUES (2, 2, 2)
 INTO BajalicaProtivMagCud (IdBaj, IdCud, IdBajalicaPMagCud) VALUES (3, 3, 3)
-INTO BajalicaProtivMagCud (IdBaj, IdCud, IdBajalicaPMagCud) VALUES (4, 4, 4)
 SELECT * FROM dual;
 
 INSERT ALL
-
-INTO CudImaMagSpos (IdCud, IdMagicnaSpos, IdCudMagSpos) VALUES (2, 2, 2)
-INTO CudImaMagSpos (IdCud, IdMagicnaSpos, IdCudMagSpos) VALUES (3, 3, 3)
+INTO CudImaMagSpos (IdCud, IdMagicnaSpos, IdCudMagSpos) VALUES (1, 1, 1)
 INTO CudImaMagSpos (IdCud, IdMagicnaSpos, IdCudMagSpos) VALUES (4, 4, 4)
-
+INTO CudImaMagSpos (IdCud, IdMagicnaSpos, IdCudMagSpos) VALUES (5, 5, 5)
 SELECT * FROM dual;
 
 INSERT ALL
 INTO CudImaSpecSpos (IdCud, IdSpecijalnaSpos, IdCudSpecSpos) VALUES (1, 1, 1)
+INTO CudImaSpecSpos (IdCud, IdSpecijalnaSpos, IdCudSpecSpos) VALUES (2, 2, 2)
+INTO CudImaSpecSpos (IdCud, IdSpecijalnaSpos, IdCudSpecSpos) VALUES (3, 3, 3)
+INTO CudImaSpecSpos (IdCud, IdSpecijalnaSpos, IdCudSpecSpos) VALUES (4, 4, 4)
 INTO CudImaSpecSpos (IdCud, IdSpecijalnaSpos, IdCudSpecSpos) VALUES (5, 5, 5)
 SELECT * FROM dual;
 
 INSERT ALL
-INTO PostojiLegenda (IdCud, IdLeg, idpostojilegenda) VALUES (1, 1,1)
-INTO PostojiLegenda (IdCud, IdLeg, idpostojilegenda) VALUES (2, 2,2)
-INTO PostojiLegenda (IdCud, IdLeg, idpostojilegenda) VALUES (3, 3,3)
-INTO PostojiLegenda (IdCud, IdLeg, idpostojilegenda) VALUES (4, 4,4)
-INTO PostojiLegenda (IdCud, IdLeg, idpostojilegenda) VALUES (5, 5,5)
+INTO PostojiLegenda (IdCud, IdLeg, idpostojilegenda) VALUES (1, 1, 1)
+INTO PostojiLegenda (IdCud, IdLeg, idpostojilegenda) VALUES (2, 2, 2)
+INTO PostojiLegenda (IdCud, IdLeg, idpostojilegenda) VALUES (3, 3, 3)
+INTO PostojiLegenda (IdCud, IdLeg, idpostojilegenda) VALUES (4, 4, 4)
+INTO PostojiLegenda (IdCud, IdLeg, idpostojilegenda) VALUES (5, 5, 5)
 SELECT * FROM dual;
 
 INSERT ALL
@@ -153,10 +158,9 @@ INTO ProtivCud (IdProtivCud, IdCud, IdPM) VALUES (5, 5, 5)
 SELECT * FROM dual;
 
 INSERT ALL
-INTO Susret (IdSusret, IdLov, ImePP, IdLok, Vreme, Ishod) VALUES (1, 1, 'Drakula', 1, '2023-05-15', 'Pobeda lovca')
-INTO Susret (IdSusret, IdLov, ImePP, IdLok, Vreme, Ishod) VALUES (2, 2, 'Griffin Reinhart', 2, '2022-09-20', 'Poraz lovca')
-INTO Susret (IdSusret, IdLov, ImePP, IdLok, Vreme, Ishod) VALUES (3, 3, 'Ariel', 3, '2024-01-10', 'Sukob izbegnut')
-INTO Susret (IdSusret, IdLov, ImePP, IdLok, Vreme, Ishod) VALUES (4, 4, 'Sladja', 4, '2023-11-05', 'Neodlučan ishod')
-INTO Susret (IdSusret, IdLov, ImePP, IdLok, Vreme, Ishod) VALUES (5, 5, 'iron Golem', 5, '2022-12-30', 'Pobeda predstavnika')
+INTO Susret (IdSusret, IdLov, ImePP, IdLok, Vreme, Ishod) VALUES (1, 1, 'Smaug', 1, '2023-06-01', 'Pobeda čudovišta')
+INTO Susret (IdSusret, IdLov, ImePP, IdLok, Vreme, Ishod) VALUES (2, 2, 'Remus', 2, '2022-10-15', 'Poraz lovca')
+INTO Susret (IdSusret, IdLov, ImePP, IdLok, Vreme, Ishod) VALUES (3, 3, 'Drakula', 3, '2023-01-20', 'Pobeda čudovišta')
+INTO Susret (IdSusret, IdLov, ImePP, IdLok, Vreme, Ishod) VALUES (4, 4, 'Ravljola', 4, '2023-03-05', 'Neodlučan ishod')
+INTO Susret (IdSusret, IdLov, ImePP, IdLok, Vreme, Ishod) VALUES (5, 5, 'Persej', 5, '2022-12-10', 'Pobeda lovca')
 SELECT * FROM dual;
-
